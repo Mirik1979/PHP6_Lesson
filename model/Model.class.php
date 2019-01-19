db8002bc26399f09d0c8b94451d3ed047fff05b1
Mirik1979 <lya-job@yandex.ru>
2019-01-19 15:16:23 +0300

    yes

---
+++ model/Model.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class Model
+{
+    public $view = 'index';
+    public $title;
+
+
+    function __construct()
+    {	
+        $this->title = Config::get('sitename');
+    }
+
+
+
+    public function index($data) 
+	{
+
+    }
+	
+	public function __call($methodName, $args) 
+	{
+    	header("Location: Config::get('domain')/page404/");
+  	}
+	
+
+
+
+
+
+
+
+}
